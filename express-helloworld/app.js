ff3000cb8500dc20311d8dfe6e4dce3cb7410374
perhall <perhall@users.noreply.github.com>
2022-01-04 00:20:43 +0100

    add /venus and more clear text
    I name thee version 2.0

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -2,11 +2,15 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!\n');
+  res.send('Hello our World 2.0!\n');
 });
 
 app.get('/mars', function (req, res) {
-  res.send('Hello Mars!\n');
+  res.send('Hello Red Mars!\n');
+});
+
+app.get('/venus', function (req, res) {
+  res.send('Hello sweet Venus!\n');
 });
 
 app.listen(8080, function () {
